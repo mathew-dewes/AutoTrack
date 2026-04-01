c55c70900396c71f918348f0a6b1d68f88321bda
Mathew Dewes <mathewdewes01@gmail.com>
2026-04-02 10:31:53 +1300

    Vehicle list added to dashboard

--- app/dashboard/_components/Dashboard.tsx
+++ app/dashboard/_components/Dashboard.tsx
@@ -2,11 +2,14 @@ import FleetSummary from "./FleetSummary";
 import Efficiencies from "./Efficiencies";
 import Trends from "./Trends";
 import VehicleDisplay from "./VehicleDisplay";
+import { VehicleCarousel } from "./VehicleCarousel";
+
 
 export default function Dashboard(){
     return (
         <div className="grid md:grid-cols-2 lg:grid-cols-3  gap-5">
             <FleetSummary/>
+            <VehicleCarousel/>
             <Efficiencies/>
             <Trends/>
             <VehicleDisplay/>
