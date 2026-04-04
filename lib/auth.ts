fd5af5f60fa33111499834b0262319800ab63be8
Mathew Dewes <mathewdewes01@gmail.com>
2026-04-05 07:39:37 +1200

    Google Oauth added with better auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,12 +3,20 @@ import { headers } from "next/headers";
 import { Pool } from "pg";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
   database: new Pool({
     connectionString: process.env.DATABASE_CONNECTION_STRING,
   }),
   emailAndPassword:{
     enabled: true
-  }
+  },
+      socialProviders: {
+        google: { 
+            prompt: "select_account", 
+            clientId: process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+        }, 
+    },
 });
 
 export async function getUserId(){
